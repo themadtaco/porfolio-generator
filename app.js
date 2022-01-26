239aa66154cc78c3a225d29285771bd59b2bca37
Drogon <aatrox48@gmail.com>
2022-01-26 07:26:17 -0600

    added inquirer to app

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
 // const printProfileData = profileDataArr => {
 //     // This...
